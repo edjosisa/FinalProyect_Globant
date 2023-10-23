cb2ff3e0e85ae82a3c95cdab54245fba723643d4
edjosisario <110759162+edjosisario@users.noreply.github.com>
2023-10-23 12:35:51 -0500

    Actulización de la clase University con los metodos para buscar un estudiante por su Id y busca una clase por el Id del estudiante.

--- AppSchool_Globant/src/app/org/globant/model/University.java
+++ AppSchool_Globant/src/app/org/globant/model/University.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 public class University {
 
+    // variables de instancia privadas en la clase "Universidad".
+
     private ArrayList<Student> sList;
     private ArrayList<Subject> suList;
     private ArrayList<TeacherFullTime> tfList;
     private ArrayList<TeacherPartTime> tpList;
 
+    // Constructor para inicializar las listas
     public University() {
         sList = new ArrayList<>();
         suList = new ArrayList<>();
@@ -16,6 +19,7 @@ public class University {
         tpList = new ArrayList<>();
     }
 
+    // Métodos getters y setters de la clase University.
     public void addStudent(Student student) {
         sList.add(student);
     }
@@ -30,6 +34,49 @@ public class University {
 
     public void addTeacherPT(TeacherPartTime teacherPartTime) {
         tpList.add(teacherPartTime);
+    }
+
+    /**
+     * La función busca un estudiante en una lista de estudiantes por su ID y
+     * devuelve el objeto de estudiante.
+     *
+     * @param id El parámetro "id" es un número entero que representa el
+     *           identificador único de un estudiante.
+     * @return El método devuelve un objeto Estudiante y en cado de que no se
+     *         encuentre devuelve un null.
+     */
+    public Student findStudentById(int id) {
+
+        for (Student student : sList) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * La función busca las clases en las que está matriculado un estudiante con un
+     * determinado ID.
+     *
+     * @param id El parámetro "id" es un número entero que representa el
+     *           identificador único de un estudiante.
+     * @return El método devuelve una ArrayList de objetos Subject.
+     */
+    public ArrayList<Subject> findClassByStudent(int id) {
+        ArrayList<Subject> classByStudent = new ArrayList<>();
+        Student student = findStudentById(id);
+
+        if (student != null) {
+            for (Subject subject : suList) {
+                if (subject.getsList().contains(student)) {
+                    classByStudent.add(subject);
+                }
+            }
+        }
+        return classByStudent;
+
     }
 
 }
