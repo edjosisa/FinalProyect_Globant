eaf28cd9799ab3f162a457fcc13f903af65d347b
edjosisario <110759162+edjosisario@users.noreply.github.com>
2023-10-24 12:42:52 -0500

    Implementación de variables y métodos para la unión entre el modelo y la vista en la clase UniversityController

--- AppSchool_Globant/src/app/org/globant/controller/UniversityController.java
+++ AppSchool_Globant/src/app/org/globant/controller/UniversityController.java
@@ -1,5 +1,54 @@
 package app.org.globant.controller;
 
-public class UniversityController {
+import app.org.globant.model.Student;
+import app.org.globant.model.Subject;
+import app.org.globant.model.University;
+import app.org.globant.view.UniversityView;
 
+public class UniversityController {
+    private University model;
+    private UniversityView view;
+
+    public UniversityController(University model, UniversityView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() {
+        int choice;
+        do {
+            view.showMenu();
+            choice = view.getInput();
+            switch (choice) {
+                case 1:
+                    view.printAllProfessors(model.getTfList(), model.getTpList());
+                    break;
+                case 2:
+                    view.printAllClasses(model.getSuList());
+                    break;
+                case 3:
+                    Student newStudent = view.createStudent();
+                    model.addStudent(newStudent);
+                    break;
+                case 4:
+                    Subject newClass = view.createClass(model.getTfList(), model.getTpList(), model.getsList());
+                    model.addSubject(newClass);
+                    break;
+                case 5:
+                    int studentId = view.StudentId();
+                    Student student = model.findStudentById(studentId);
+                    if (student != null) {
+                        view.showClassesByStudent(model.findClassByStudent(studentId));
+                    } else {
+                        System.out.println("Student not found");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Bye");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        } while (choice != 6);
+    }
 }
